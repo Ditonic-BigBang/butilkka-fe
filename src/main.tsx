67b8092488e9d54879a90a858c6c4c0cb509a401
Seo Minsu <minsu9812@naver.com>
2026-06-29 14:37:30 +0900

    [feat] TanStack Query·Zustand 데이터 레이어 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { queryClient } from '@/lib/queryClient'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {/* devtools는 프로덕션 번들에서 자동으로 제외됨 */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
